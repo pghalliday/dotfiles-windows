e7de4b7ec56c210c7b4abbaad1280006bd44a088
Peter Halliday <pghalliday@gmail.com>
2015-03-10 17:33:17 +0100

    install 7-zip

---
+++ cookbooks/7-zip/metadata.rb
@@ -0,0 +1,3 @@
+name '7-zip'
+version '0.1.0'
+depends 'windows'
---
+++ cookbooks/7-zip/recipes/default.rb
@@ -0,0 +1,5 @@
+windows_package '7-Zip 9.20 (x64 edition)' do
+  source 'http://www.7-zip.org/a/7z920-x64.msi'
+  checksum '62df458bc521001cd9a947643a84810ecbaa5a16b5c8e87d80df8e34c4a16fe2'
+  action :install
+end
